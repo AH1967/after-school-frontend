bafe89d1515fa9f9ebede8b4661b6109be7810c9
Abdalla Hussein <AH1967@LIVE.MDX.AC.UK>
2024-12-06 02:06:09 +0400

    Add data binding example to app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        count: 0
     },
     methods: {
         showAlert() {
             alert('Button clicked!');
+        },
+        increment() {
+            this.count += 1;
         }
     }
 });
