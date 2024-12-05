2e5ba8fede1ee9ff6b8222f2c47cc3e50eecaf73
Abdalla Hussein <AH1967@LIVE.MDX.AC.UK>
2024-12-06 02:04:22 +0400

    Add event handling to app.js

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
+    },
+    methods: {
+        showAlert() {
+            alert('Button clicked!');
+        }
     }
 });
